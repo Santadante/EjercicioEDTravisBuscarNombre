d46d2eb75a566a6e2b724515bd8c3c6e3e4b9a59
Santadante <49271860+Santadante@users.noreply.github.com>
2020-04-30 12:12:56 +0200

    mostrar errores

--- test/EjercicioTest.js
+++ test/EjercicioTest.js
@@ -7,4 +7,6 @@ var func = require('../Ejercicio.js');
 // Test
 it('comprobar función ejercicio', function() {
 	assert.equal(func.MiNombre("Juan\nPedro\nDavid\nErnesto"), "David, tu nombre se encuentra en la tercera línea");
+	assert.equal(func.MiNombre("Juan\nPedro\nAlfonso\nErnesto"), "David, tu nombre no se encuentra en la tercera línea");
+	assert.equal(func.MiNombre("Juan\nPedro\nRoberto\nErnesto"), "David, tu nombre se encuentra en la tercera línea");
 });
